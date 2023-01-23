f4951ef324961d226e0db2a95f4665a9af5be510
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-22 21:12:53 -0300

    feat: created category

--- backend/src/routes/categories.router.ts
+++ backend/src/routes/categories.router.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  return response.status(201).json({ category });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
