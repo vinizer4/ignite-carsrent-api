45f56cdd8d3dad10a1b080b3f40822cb914a3914
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-01-23 01:07:49 -0300

    feat: created backend

--- backend/src/routes/categories.router.ts
+++ backend/src/routes/categories.router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import multer from "multer";
 
 const categoriesRoutes = Router();
+
+const upload = multer();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
